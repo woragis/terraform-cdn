746b55350fcae14bb3ee1db7baaee647e6538d19
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-22 10:30:09 -0300

    Update terraform cognito module and action

--- locals.tf
+++ locals.tf
@@ -1,21 +1,6 @@
 locals {
-  aws_region         = var.aws_region
-  root_domain        = var.root_domain
-  full_domain        = "${var.subdomain}.${var.root_domain}"
-  environment        = lower(var.environment)
-  cloudfront_origin_id = "${replace(local.full_domain, ".", "-")}-origin"
+  aws_region        = var.aws_region
 
-  cert_validation = tolist(aws_acm_certificate.cert.domain_validation_options)[0]
-
-  common_tags = merge(
-    {
-      Project     = coalesce(trimspace(var.project_name), "App")
-      Environment = local.environment
-      ManagedBy   = "Terraform"
-      Purpose     = "Static Website"
-    },
-    var.tags != null ? var.tags : {}
-  )
-
-  ssl_minimum_protocol_version = var.ssl_minimum_protocol_version
+  cognito_name      = var.cognito_name
+  cognito_user_pool = var.cognito_user_pool
 }
--- variables.tf
+++ variables.tf
@@ -1,53 +1,15 @@
-variable "project_name" {
-  type        = string
-  description = "Project name used for naming resources and tags"
-  default     = null
-}
-
-variable "root_domain" {
-  type        = string
-  description = "Main domain for the application (e.g., 'example.com')"
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\.[a-zA-Z]{2,}$", var.root_domain))
-    error_message = "The root_domain must be a valid domain name (e.g., example.com)."
-  }
-}
-
-variable "subdomain" {
-  type        = string
-  description = "Subdomain for the app (e.g., 'app' -> app.example.com)"
-  default     = "app"
-}
-
 variable "aws_region" {
   type        = string
   description = "AWS region to deploy all resources"
   default     = "us-east-1"
 }
 
-variable "environment" {
-  type        = string
-  description = "Deployment environment (e.g., 'dev', 'prod', 'staging')"
-  default     = "dev"
-  validation {
-    condition     = contains(["dev", "prod", "staging"], lower(var.environment))
-    error_message = "The environment must be one of: 'dev', 'prod', or 'staging'."
-  }
-}
-
-variable "origin_domain_name" {
+variable "cognito_name" {
   type        = string
   description = "Domain name of the S3 bucket origin for CloudFront (e.g., 'app-bucket.s3.amazonaws.com')"
 }
 
-variable "tags" {
-  type        = map(string)
-  description = "Common tags to apply to all resources"
-  default     = {}
-}
-
-variable "ssl_minimum_protocol_version" {
+variable "cognito_user_pool" {
   type        = string
-  description = "Minimum protocol version for SSL in CloudFront"
-  default     = "TLSv1.2_2021"
+  description = "Domain name of the S3 bucket origin for CloudFront (e.g., 'app-bucket.s3.amazonaws.com')"
 }
